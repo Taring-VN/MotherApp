305b419803fc314ab46e3fb89ce7fd48dbb9a214
Anh Dan <anhdan.do@gmail.com>
2022-11-22 14:58:49 +0700

    init

---
+++ MotherApp/AnhViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AnhViewController.swift
+//  DemoX
+//
+//  Created by Nam Nguyễn on 21/11/2022.
+//
+
+import UIKit
+import iCOM_Service
+
+class AnhViewController: UIViewController {
+
+    @IBAction func navigateToICOM(_ sender: Any) {
+//        let vc = ControlDeviceVC
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
